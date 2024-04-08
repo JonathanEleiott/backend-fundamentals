545e28fe2a76e71e2ea36195a4c64ef551008e50
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-04-08 13:01:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Servers (computer that takes in requests from the internet)
+
+- takes in a request from the frontend
+- OPTIONAL: can make a request to a DB if necessary
+- responds to the frontend
+
+## HTTP Requests Type (fetch call) -> CRUD (Create, Read, Update, Delete)
+
+- Create -> POST -> add data
+- Read -> GET -> retrieves data
+- Update -> PUT/PATCH -> modify data (completely update vs partially update)
+- Delete -> DELETE -> Remove data
+
+## OLD WAY
+
+- if/else statements to manually check the method and endpoint
+- very clogged
+- no security built in (spinner if endpoint doesn't exist)
+
+## Express
+
+- very clean
+- more security built in
+- requires npm
+
+## Routes
+
+- app.get('path', (req, res) => { res.send('<h1>Welcome</h1>') })
+- data -> /api/v1/endpoint
+- HTML -> /endpoint
+
+## Route Parameters
+
+- used for getting specific items
+- :variableName (separated by /)
+- req.params -> object with kes and values
+
+## Queries
+
+- used generally in filters with optional parameters
+- ?key=value&key2=value2 (separated by &)
+- req.query -> object with keys and values
+
+## NPM (Node Package Manager)
+
+- npm init -y (creates packge.json)
+- npm i express
+
+## Notes
+
+- node uses module.exports and require instead of export and import
+- app.listen takes in an optional second parameter (function that will run when the server starts)
+- the function in a route will take in a request and a response
+  - request -> from the frontend (usually the browser)
+  - response -> what the server sends back to the frontend
+
---
+++ server.js
@@ -0,0 +1,89 @@
+// OLD WAY
+// const http = require('http');
+
+// http.createServer((req, res) => {
+//   if(req.method === 'GET' && req.url === '/') {
+//     res.end(`
+//       <h1>Welcome</h1>
+//     `)
+//   } else if (req.method === 'GET' && req.url ==='/puppies') {
+//     res.end(`
+//       <h1>Puppies!</h1>
+//     `)
+//   } else {
+//     res.end(`
+//       <h1>Page does not exist</h1>
+//     `)
+//   }
+// }).listen(8080);
+
+// Express
+const express = require('express');
+const app = express();
+
+const bunnies = [
+  { name: 'Floppy', color: 'brown'}, 
+  { name: 'Thumper', color: 'gray'},
+  { name: 'Rebecca', color: 'rebecca purple' }
+]
+
+const puppies = [
+  { name: 'Spot', color: 'black'}, 
+  { name: 'Dodger', color: 'dodgerblue'},
+  { name: 'Buddy', color: 'black' }
+]
+
+app.get('/', (req, res) => {
+  console.log('request made to / route');
+
+
+  res.send(`
+    <h1>Welcome</h1>
+
+    <h2>bunnies r awesome!</h2>
+
+    <script>
+      console.log('I like bunnies!');
+    </script>
+  `);
+});
+
+app.get('/api/v1/bunnies', (req, res) => {
+  res.send(bunnies);
+});
+
+app.get('/api/v1/bunnies/:animalName/:animalColor', (req, res) => {
+  console.log(`REQ PARAMS:`, req.params);
+  const { animalName, animalColor } = req.params;
+
+  const foundBunny = bunnies.find((bunny) => {
+    return bunny.name === animalName && bunny.color === animalColor
+  })
+
+  res.send(foundBunny);
+});
+
+app.get('/api/v1/puppies', (req, res) => {
+  let foundPuppies = puppies;
+
+  console.log(`REQ QUERY`, req.query);
+  if(req.query.color) {
+    foundPuppies = puppies.filter((puppy) => {
+      return puppy.color === req.query.color;
+    })
+  }
+
+  res.send(foundPuppies);
+})
+
+// app.get('/api/v1/bunnies/:animalFood/:animalToy', (req, res) => {
+//   res.send('another route');
+// })
+
+app.post('/animals', (req, res) => {
+  // add new animal
+})
+
+app.listen(8080, () => {
+  console.log(`listening on port 8080`);
+});
